9cbe105ee8327640b2595c8b184dae4ce2d55090
Shadow <Shadow@SHADOW-2H84K20E>
2020-03-09 00:21:22 +0100

    change

---
+++ mybodypartner/src/main/java/fr/univ/lille/fil/mybodypartner/MybodypartnerApplication.java
@@ -0,0 +1,13 @@
+package fr.univ.lille.fil.mybodypartner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybodypartnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybodypartnerApplication.class, args);
+	}
+
+}
---
+++ mybodypartner/src/test/java/fr/univ/lille/fil/mybodypartner/MybodypartnerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univ.lille.fil.mybodypartner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybodypartnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
