41d7ef789364e9888d7186a8b7e226f6cf08a338
Shadow <Shadow@SHADOW-2H84K20E>
2020-03-11 22:58:12 +0100

    configure as war rest-service

---
+++ mbp-rest-service/src/main/java/fr/univ/lille/fil/mbprestservice/SpringBootTomcatApplication.java
@@ -0,0 +1,21 @@
+package fr.univ.lille.fil.mbprestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootTomcatApplication extends SpringBootServletInitializer {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTomcatApplication.class, args);
+	}
+
+	@Override
+    protected SpringApplicationBuilder configure(
+      SpringApplicationBuilder builder) {
+        return builder.sources(SpringBootTomcatApplication.class);
+    }
+ 
+}
---
+++ mbp-rest-service/src/main/java/fr/univ/lille/fil/mbprestservice/controller/TomcatController.java
@@ -0,0 +1,18 @@
+package fr.univ.lille.fil.mbprestservice.controller;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TomcatController {
+ 
+    @GetMapping("/")
+    public Collection<String> sayHello() {
+        return IntStream.range(0, 10)
+          .mapToObj(i -> "Hello number " + i)
+          .collect(Collectors.toList());
+    }
+}
