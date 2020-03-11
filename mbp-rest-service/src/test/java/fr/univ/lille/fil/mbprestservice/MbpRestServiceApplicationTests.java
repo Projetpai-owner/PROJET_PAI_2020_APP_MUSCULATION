bd39c7e7ba27854cad19f4ed5f04c27f93b3c2ef
Shadow <Shadow@SHADOW-2H84K20E>
2020-03-11 21:36:36 +0100

    add files

---
+++ mbp-rest-service/src/test/java/fr/univ/lille/fil/mbprestservice/MbpRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univ.lille.fil.mbprestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbpRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
