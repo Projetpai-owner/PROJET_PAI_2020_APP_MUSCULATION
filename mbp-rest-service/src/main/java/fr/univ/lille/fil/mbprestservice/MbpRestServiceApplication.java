8aa6739a961bb3c2f9ff363e8c855081c7be651a
Shadow <Shadow@SHADOW-2H84K20E>
2020-03-11 21:42:14 +0100

    add Application main

---
+++ mbp-rest-service/src/main/java/fr/univ/lille/fil/mbprestservice/MbpRestServiceApplication.java
@@ -0,0 +1,13 @@
+package fr.univ.lille.fil.mbprestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbpRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbpRestServiceApplication.class, args);
+	}
+
+}
