259698ff84f251c3182950dae94511a3963e0c99
Clément Salvador <clementsalvador.isn@gmail.com>
2020-03-22 17:15:32 +0100

    Ajout navbar + debut carousel

---
+++ mbp-webapp/src/app/acceuil-carousel/acceuil-carousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-acceuil-carousel',
+  templateUrl: './acceuil-carousel.component.html',
+  styleUrls: ['./acceuil-carousel.component.scss']
+})
+export class AcceuilCarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- mbp-webapp/src/app/app.module.ts
+++ mbp-webapp/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AcceuilCarouselComponent } from './acceuil-carousel/acceuil-carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcceuilComponent,
+    NavbarComponent,
+    AcceuilCarouselComponent,
   ],
   imports: [
     BrowserModule,
