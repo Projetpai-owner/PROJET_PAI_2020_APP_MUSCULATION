dd30f1467b9c0c0dc0f8ed2f567e06f16adfb69f
Theo <theo.declercks@hotmail.fr>
2020-03-24 00:09:57 +0100

    Modif entite pour que ça marche pour toi Rémy

--- mbp-rest-service/src/main/java/fr/univ/lille/fil/mbprestservice/entity/User.java
+++ mbp-rest-service/src/main/java/fr/univ/lille/fil/mbprestservice/entity/User.java
@@ -6,14 +6,16 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="personne")
+@Table(name = "personne")
 public class User {
-	
+
+	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int pid;
 	private String firstName;
@@ -22,83 +24,90 @@ public class User {
 	private String sex;
 	private String email;
 	private String password;
-	
-	@ManyToOne(cascade = {CascadeType.ALL})
+
+	@ManyToOne(cascade = { CascadeType.ALL })
 	@JoinColumn(name = "sid")
-	private int sid;
-	
-	@ManyToOne(cascade = {CascadeType.ALL})
+	private Gym sid;
+
+	@ManyToOne(cascade = { CascadeType.ALL })
 	@JoinColumn(name = "adresse")
-	private int adresse;
-	
+	private Place adresse;
+
 	public String getFirstName() {
 		return firstName;
 	}
-	
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-	
+
 	public String getLastName() {
 		return lastName;
 	}
-	
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	
+
 	public Date getBornDate() {
 		return bornDate;
 	}
-	
+
 	public void setBornDate(Date bornDate) {
 		this.bornDate = bornDate;
 	}
-	
+
 	public String getSex() {
 		return sex;
 	}
-	
+
 	public void setSex(String sex) {
 		this.sex = sex;
 	}
-	
+
 	public String getEmail() {
 		return email;
 	}
-	
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
 	public String getPassword() {
 		return password;
 	}
-	
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "User [firstName=" + firstName + "; lastName=" + lastName + "; bornDate= " 
-				+ bornDate.toString() + "; sex=" + sex + "; email=" + email + "; password=" 
-				+ password + "]";
+		return "User [firstName=" + firstName + "; lastName=" + lastName + "; bornDate= " + bornDate.toString()
+				+ "; sex=" + sex + "; email=" + email + "; password=" + password + "]";
 	}
 
-	public int getSid() {
+	public int getPid() {
+		return pid;
+	}
+
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+
+	public Gym getSid() {
 		return sid;
 	}
 
-	public void setSid(int sid) {
+	public void setSid(Gym sid) {
 		this.sid = sid;
 	}
 
-	public int getAdresse() {
+	public Place getAdresse() {
 		return adresse;
 	}
 
-	public void setAdresse(int adresse) {
+	public void setAdresse(Place adresse) {
 		this.adresse = adresse;
 	}
 
