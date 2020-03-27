da19ac1d9f5d4aa9d8c19eed9f209190f408246c
Anthony Bliecq <anthony.bliecq.etu@univ-lille.fr>
2020-03-27 16:11:35 +0100

    liaison formulaire inscription accueil

--- mbp-webapp/src/app/app-routing.module.ts
+++ mbp-webapp/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InscriptionComponent } from './inscription/inscription.component';
-import { AppComponent } from './app.component';
+import { AcceuilComponent } from './acceuil/acceuil.component';
 
 
-const routes: Routes = [
+export const routes: Routes = [
+  {path: '', component: AcceuilComponent},
   {path: 'signUp', component: InscriptionComponent}
 
 ];
--- mbp-webapp/src/app/app.module.ts
+++ mbp-webapp/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'  
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InscriptionComponent } from './inscription/inscription.component';
