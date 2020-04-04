c650ba3fa8529d5dc5e716cc6f3b4768b7778cc1
Anthony Bliecq <anthony.bliecq.etu@univ-lille.fr>
2020-04-04 19:02:22 +0200

    granted all getAllSalles

--- mbp-rest-service/src/main/java/fr/univ/lille/fil/mbprestservice/security/SpringSecurityConfig.java
+++ mbp-rest-service/src/main/java/fr/univ/lille/fil/mbprestservice/security/SpringSecurityConfig.java
@@ -38,7 +38,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.csrf().disable()
-        .authorizeRequests().antMatchers("/login").permitAll()
+        .authorizeRequests().antMatchers("/login","/getAllSalles").permitAll()
         .anyRequest().authenticated()
         .and().sessionManagement()
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
