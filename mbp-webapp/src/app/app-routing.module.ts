7c1080f2f6b45ca557a4bba6666f265947ce0fdf
bliecqa <antho59.bliecq@gmail.com>
2020-04-11 23:56:01 +0200

    Verif admin (#14)
    * update gitignore
    
    * ajout verif admin
    
    Co-authored-by: Shadow <Shadow@SHADOW-S51MVOT3>
    Co-authored-by: Projetpai-owner <60009438+Projetpai-owner@users.noreply.github.com>

--- mbp-webapp/src/app/app-routing.module.ts
+++ mbp-webapp/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { InscriptionComponent } from './inscription/inscription.component';
 import { AcceuilComponent } from './acceuil/acceuil.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth/auth.guard';
 import { FormCreationAnnonceComponent } from './form-creation-annonce/form-creation-annonce.component';
 
 
 export const routes: Routes = [
   {path: '', component: AcceuilComponent},
-  {path: 'signUp', component: InscriptionComponent},
+  {path: 'signUp', component: InscriptionComponent,canActivate:[AuthGuard]},
   {path: 'signIn', component: LoginComponent},
   {path: 'createAdvert', component: FormCreationAnnonceComponent}
 
