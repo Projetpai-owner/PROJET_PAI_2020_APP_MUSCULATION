fe5b34ede6ea48d3c3b24bdda5c134f294e76382
Clément Salvador <clementsalvador.isn@gmail.com>
2020-05-31 18:46:38 +0200

    Première version du formulaire de filtre d'annonce

--- mbp-webapp/src/app/advert-list/advert-list.component.ts
+++ mbp-webapp/src/app/advert-list/advert-list.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import {AdvertService} from '../services/Advert.service';
 import {AdvertItemList} from '../models/AdvertItemList.model';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { TypeSeance } from '../models/TypeSeance.model';
+import { TypeSeanceService } from '../services/TypeSeance.service';
+import { SalleService } from '../services/Salle.service';
+import { Salle } from '../models/Salle.model';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-advert-list',
@@ -11,13 +17,45 @@ import { Router } from '@angular/router';
 export class AdvertListComponent implements OnInit {
 
   ItemsArray = [];
-
-  constructor(public advertService: AdvertService, private router: Router) { }
+  obsTypeSeance: Observable<TypeSeance[]>;
+  obsSalles: Observable<Salle[]>;
+  filtreAnnonceForm: FormGroup;
+  zoneFiltreVisible: boolean;
+  
+  constructor(public advertService: AdvertService, 
+              private typeSeanceService: TypeSeanceService,
+              private salleService: SalleService,
+              private router: Router,
+              private formBuilder: FormBuilder,) { }
 
   ngOnInit(): void {
     this.advertService.getAdverts().subscribe((res: AdvertItemList[]) => {
       this.ItemsArray = res;
     });
+    this.initSelect();
+    this.initForm();
+    this.zoneFiltreVisible = false;
+  }
+
+  initSelect(): void {
+    this.obsTypeSeance = this.typeSeanceService.getTypeSeance();
+    this.obsSalles = this.salleService.getSalles();
+  }
+
+  initForm(): void{
+    this.filtreAnnonceForm = this.formBuilder.group({
+      date: ['',null],
+      dureeMin: ['',null],
+      dureeMax: ['',null],
+      niveau: ['',null],
+      typeSeance: ['',null],
+      salle: ['',null],
+      sex: ['',null]
+    });
+  }
+
+  showZoneFiltre(): void{
+    this.zoneFiltreVisible = !this.zoneFiltreVisible;
   }
 
   deleteAdvertById(aid: number){
